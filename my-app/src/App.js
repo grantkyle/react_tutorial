23611938d7082c36410900f00abe25422d83375e
Grant Kyle <sgrantkyle@gmail.com>
2020-04-08 09:18:59 -0500

    setting up App.js and Todos.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,31 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+import './App.css';
+
 class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Hit the Skreets',
+                completed: false
+            },{
+                id: 2,
+                title: 'Give old lady a suck job',
+                completed: false
+            },{
+                id: 3,
+                title: 'Try PCP for first time',
+                completed: false
+            },
+        ]
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>To-do List</h1>
+                <Todos />
             </div>
         );
     }
