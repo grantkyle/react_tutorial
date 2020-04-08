a28d161f168a12840111d8565cb90333ffa3b573
Grant Kyle <sgrantkyle@gmail.com>
2020-04-08 16:26:15 -0500

    added routing with links in header, added axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
@@ -9,21 +11,11 @@ import './App.css';
 class App extends Component {
     state = {
         todos: [
-            {
-                id: uuidv4(),
-                title: 'Hit the Skreets',
-                completed: false
-            }, {
-                id: uuidv4(),
-                title: 'Give old lady a suck job',
-                completed: true
-            }, {
-                id: uuidv4(),
-                title: 'Try PCP for first time',
-                completed: false
-            },
+
         ]
     }
+
+    componentDidMount
     // Toggle complete off and on
     toggleComplete = (id) => {
         this.setState({
@@ -52,19 +44,26 @@ class App extends Component {
             completed: false
         }
 
-        this.setState ({ todos: [...this.state.todos, newTodo] })
+        this.setState({ todos: [...this.state.todos, newTodo] })
     }
 
     render() {
         return (
-            <div className="App">
-                <div className="container">
-                    <Header />
-                    <AddTodo addTodo={this.addTodo}/>
-                    <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
-                        deleteTodo={this.deleteTodo} />
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Header />
+                        <Route path="/" render={props => (
+                            <React.Fragment>
+                                <AddTodo addTodo={this.addTodo} />
+                                <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
+                                    deleteTodo={this.deleteTodo} />
+                            </React.Fragment>
+                        )} />
+                        <Route path="/about" component={About} />
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
