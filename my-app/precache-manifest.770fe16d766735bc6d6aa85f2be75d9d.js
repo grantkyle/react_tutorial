fc1ff7b26de63c14f7c65914dd0ca382ac91e0e9
Grant Kyle <sgrantkyle@gmail.com>
2020-04-09 14:47:50 -0500

    heroku deploy stuffs

---
+++ my-app/precache-manifest.770fe16d766735bc6d6aa85f2be75d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf886814ee53b7515c96a234bcdefd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4e68fd22f2425c45ea",
+    "url": "/static/css/main.1f1b1d44.chunk.css"
+  },
+  {
+    "revision": "68c62b0e4f442c14aeca",
+    "url": "/static/js/2.ee98db90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee98db90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4e68fd22f2425c45ea",
+    "url": "/static/js/main.96b833ee.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
